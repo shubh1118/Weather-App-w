a2d825d146a454fc44c21616499a4198c1dc51fe
Shubham Chaurasia <chaurasiashubham65@gmail.com>
2023-10-13 10:46:15 +0530

    First Class

---
+++ my-react-app/src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import HomeComponents from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeComponents />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
